7b16b952eacb30000fb3255cd6742aafc5d24199
Nicole Lucien <nicolemlucien@gmail.com>
2019-11-11 11:54:32 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,19 @@
 class Person
   
-  def initialize(attributes_hash)
+  def initialize(:name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, 
+:wrist_size, :glove_size, :pant_length, :pant_width)
     
-  end 
+    @name = name 
+    @birthday = birthday 
+    @hair_color = hair_color
+    @eye_color = eye_color
+    @height = height
+    @weight = weight
+    @handed = handed
+    @complexion = complexion
+    @t_shirt_size = t_shirt_size
+    @wrist_size = 
+    end 
   
   
 end
